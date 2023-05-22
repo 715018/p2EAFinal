4798e713205967379a87c4ab4e7f77fca3e890fd
715018 <715018@student.dusd.net>
2023-05-21 21:02:38 -0700

    tried stuff

--- EAP2Ayala/Assets/Scripts/PlayerControl.cs
+++ EAP2Ayala/Assets/Scripts/PlayerControl.cs
@@ -5,30 +5,60 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
+  
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    public GameObject projectilePrefab;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+      
+
+        currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
         Vector2 move = new Vector2(horizontal, vertical);
 
+        
+
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
+
+       
     }
 
     void FixedUpdate()
@@ -40,6 +70,22 @@ public class PlayerControl : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+    }
+
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
@@ -47,6 +93,8 @@ public class PlayerControl : MonoBehaviour
         Projectile projectile = projectileObject.GetComponent<Projectile>();
         
 
-        
+
     }
+
 }
+
--- EAP2Ayala/Assets/Scripts/Projectile.cs
+++ EAP2Ayala/Assets/Scripts/Projectile.cs
@@ -28,6 +28,8 @@ public class Projectile : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        
+
         Destroy(gameObject);
     }
 }
